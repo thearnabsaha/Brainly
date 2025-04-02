2b2d85325e62f1fd8ac59d7805d97788d3a6cb14
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-02 12:34:47 +0530

    feat: enhance Sidebar with navigation functionality and update SidebarItems component

--- brainly-client/src/Components/Sidebar.tsx
+++ brainly-client/src/Components/Sidebar.tsx
@@ -1,15 +1,17 @@
 import { LuBrain } from "react-icons/lu";
-import {HiHashtag,HiLink} from "react-icons/hi";
+import {HiHashtag,HiLink, HiOutlineDocumentText} from "react-icons/hi";
 import { SlSocialTwitter,SlSocialYoutube } from "react-icons/sl";
 import SidebarItems from "../Components/SidebarItems";
+import { useNavigate } from "react-router-dom";
 const Sidebar = () => {
+  const navigate=useNavigate()
   return (
     <div className="w-72 bg-white h-screen pt-5">
             <h1 className="text-3xl flex font-bold p-5 gap-2"><LuBrain fontSize={40} className="text-ppurple-600"/>Brainly</h1>
             <div className=" mt-5">
-                <SidebarItems text="Tweets" icon={<SlSocialTwitter/>}/>
-                <SidebarItems text="Videos" icon={<SlSocialYoutube/>}/>
-                {/* <SidebarItems text="Documents" icon={<HiOutlineDocumentText/>}/> */}
+                <SidebarItems text="Dashboard" icon={<HiOutlineDocumentText/>} onclick={()=>navigate("/dashboard")}/>
+                <SidebarItems text="Tweets" icon={<SlSocialTwitter/>} onclick={()=>navigate("/tweets")}/>
+                <SidebarItems text="Videos" icon={<SlSocialYoutube/>} onclick={()=>navigate("/videos")}/>
                 {/* <SidebarItems text="Links" icon={<HiLink/>}/> */}
                 <SidebarItems text="Tags" icon={<HiHashtag/>}/>
             </div>
--- brainly-client/src/Components/SidebarItems.tsx
+++ brainly-client/src/Components/SidebarItems.tsx
@@ -2,10 +2,11 @@ import { ReactElement } from "react";
 interface sidebarItemsInterface{
     text:string,
     icon:ReactElement,
+    onclick?:()=>void
 }
-const SidebarItems = ({text,icon}:sidebarItemsInterface) => {
+const SidebarItems = ({text,icon,onclick}:sidebarItemsInterface) => {
   return (
-    <p className="flex items-center text-xl gap-2 mx-2 my-3 px-5 py-2 cursor-pointer hover:bg-purple-50 rounded-md">{icon}{text}</p>
+    <p className="flex items-center text-xl gap-2 mx-2 my-3 px-5 py-2 cursor-pointer hover:bg-purple-50 rounded-md" onClick={onclick}>{icon}{text}</p>
   )
 }
 
