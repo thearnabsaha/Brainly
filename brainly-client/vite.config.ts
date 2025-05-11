8bca15770fb5bd9b831e401379cf6310530a7ce7
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-11 23:42:45 +0530

    feat: update API proxy target to use external server IP

--- brainly-client/vite.config.ts
+++ brainly-client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'http://4.240.113.255:3000/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       }
