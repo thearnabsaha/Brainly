f1642ec780cc1d078515f5cda8943242c61b5e12
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-13 02:29:04 +0530

    feat: add unique keys to tags in Navbar, Dashboard, Tweets, and Videos components

--- brainly-client/src/pages/Videos.tsx
+++ brainly-client/src/pages/Videos.tsx
@@ -129,9 +129,9 @@ const Youtube = () => {
                             <Input placeholder="Title" type="text" className="my-4" value={inputValue.title} onChange={(e) => setInputValue({ ...inputValue, title: e.target.value })} />
                             <Input placeholder="Link" type="text" value={inputValue.link} onChange={(e) => setInputValue({ ...inputValue, link: e.target.value })} />
                             <div className="h-[300px] border my-4 overflow-x-hidden sm:w-[470px] overflow-y-scroll">
-                              {tags.map((e) => {
+                              {tags.map((e,i) => {
                                 return (
-                                  <Badge className="m-2">{e} <span className="text-red-500 text-lg" onClick={() => handleFilterTags(e)}>x</span> </Badge>
+                                  <Badge className="m-2" key={e+i}>{e} <span className="text-red-500 text-lg" onClick={() => handleFilterTags(e)}>x</span> </Badge>
                                 )
                               })}
                             </div>
