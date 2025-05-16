bcd3d2617a0f415e34cb3e6c9b53cf4a8f73334b
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-16 16:32:58 +0530

    fix: update CORS configuration to use environment variable for origin

--- brainly-server/app/index.ts
+++ brainly-server/app/index.ts
@@ -17,7 +17,14 @@ app.use(
   morgan(morganFormat)
 );
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
+app.options('*', cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
