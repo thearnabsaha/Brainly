cfe49208ea230222e9f039f3090f2003d92fa45b
thearnabsaha <thearnabsaha201@gmail.com>
2025-05-16 16:25:07 +0530

    fix: simplify CORS configuration by removing unnecessary options

--- brainly-server/app/index.ts
+++ brainly-server/app/index.ts
@@ -17,10 +17,7 @@ app.use(
   morgan(morganFormat)
 );
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
